b4b11d5457521f88e5b3397fb801267d67f7fce8
Wesley Webster <wesleyadwebster@gmail.com>
2022-02-27 17:37:01 -0600

    Time: 90 ms (70.65%), Space: 46.5 MB (19.49%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.js
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.js
@@ -10,28 +10,6 @@
  * @param {TreeNode} root
  * @return {boolean}
  */
-// var isValidBST = function(root) {
-//     if (!root) true;
- 
-//     return isBST(root);
-// };
-
-// const isBST = (node, min = -Infinity, max = Infinity) => {
-//     if (!node) return true;
-    
-//     // base case
-//     if (node.value <= min || node.value > max) {
-//         return false;
-//     };
-    
-//         if ((node.left && node.value <= min) || (node.right && node.value > max)) {
-//         return false;
-//     };
-    
-    
-//     return isBST(node.left, min, node.value) && isBST(node.right, node.value, max);
-    
-// };
 
 const isValidBST = (root) => dfs(root, null, null);
 
