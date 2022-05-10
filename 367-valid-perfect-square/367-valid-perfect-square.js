5655200860270ad1c02b65af45c4a52a5593ca5e
Wesley Webster <wesleyadwebster@gmail.com>
2022-05-10 00:21:53 -0500

    Time: 67 ms (74.37%), Space: 41.7 MB (79.08%) - LeetHub

---
+++ 367-valid-perfect-square/367-valid-perfect-square.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} num
+ * @return {boolean}
+ */
+var isPerfectSquare = function(num) {
+     let p1 = 1;
+	// Split half of values that need to be checked. squared of num/2 will either be equal or greater then num
+    let p2 = Math.ceil(num/2);
+    while (p1 <= p2) {
+        let middle = p1 + Math.floor((p2 - p1)/2);
+        if (middle * middle === num) return true;
+        if (middle * middle < num) p1 = middle + 1;
+        else
+            p2 = middle - 1;
+    }
+    return false;
+};
