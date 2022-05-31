10bef4ff4571f228eb445b025128520449d83368
Wesley Webster <wesleyadwebster@gmail.com>
2022-05-31 12:33:37 -0500

    Time: 121 ms (15.70%), Space: 44.8 MB (46.79%) - LeetHub

--- 867-transpose-matrix/867-transpose-matrix.js
+++ 867-transpose-matrix/867-transpose-matrix.js
@@ -24,6 +24,7 @@ var transpose = function (matrix) {
     let result = [];
     let row = matrix.length;
     let col = matrix[0].length;
+    
     for (let i = 0; i < row; i++) {
         for (let j = 0; j < col; j++) {
             if (!result[j]) {
