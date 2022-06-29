d3a311206492cbe941e8645245ae0cbafd89c439
Wesley Webster <wesleyadwebster@gmail.com>
2022-06-28 21:18:28 -0500

    Time: 142 ms (50.18%), Space: 52 MB (37.96%) - LeetHub

---
+++ 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.js
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} k
+ * @return {boolean}
+ */
+var findTarget = function(root, k) {
+    let result = [];
+    inOrder(root, result);
+    let low = 0;
+    let high = result.length - 1;
+    
+    while (low < high) {
+          if(result[low] + result[high] === k) {
+              return true;
+          }
+           else if (result[low] + result[high] > k) {
+            high--;
+          }
+           else { 
+            low++;
+          };
+        };
+    
+    return false;
+};
+
+function inOrder(root, result) {
+    if (root !== null) {
+        inOrder(root.left, result);
+        result.push(root.val);
+        inOrder(root.right, result);
+    };
+};
