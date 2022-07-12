e61a2055699d8ccc79ceaa44438cc927f104f8a7
Wesley Webster <wesleyadwebster@gmail.com>
2022-07-11 21:55:24 -0500

    Time: 68 ms (91.22%), Space: 44.4 MB (19.92%) - LeetHub

--- 103-binary-tree-zigzag-level-order-traversal/103-binary-tree-zigzag-level-order-traversal.js
+++ 103-binary-tree-zigzag-level-order-traversal/103-binary-tree-zigzag-level-order-traversal.js
@@ -38,3 +38,31 @@ var zigzagLevelOrder = function (root) {
 
     return results;
 };
+
+// DFS (Iterative)
+// const zigzagLevelOrder = function(root) {
+//   const results = []
+
+//   const stack = [{ current: root, level: 0 }]
+
+//   while (stack.length > 0) {
+//     const { current, level } = stack.pop()
+
+//     if (!current) {
+//       continue
+//     }
+
+//     if (level >= results.length) {
+//       results.push([current.val])
+//     } else if (level % 2 === 0) {
+//       results[level].unshift(current.val)
+//     } else {
+//       results[level].push(current.val)
+//     }
+
+//     stack.push({ current: current.left, level: level + 1 })
+//     stack.push({ current: current.right, level: level + 1 })
+//   }
+
+//   return results
+// }
