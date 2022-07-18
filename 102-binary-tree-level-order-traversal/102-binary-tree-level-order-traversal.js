59c3877e1805d65ff928b62f1d2958d1c9cb86e6
Wesley Webster <wesleyadwebster@gmail.com>
2022-07-18 13:38:27 -0500

    Time: 116 ms (20.18%), Space: 44.6 MB (15.75%) - LeetHub

--- 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.js
+++ 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.js
@@ -11,25 +11,28 @@
  * @return {number[][]}
  */
 var levelOrder = function (root) {
-    if (!root) return []
+    // instantiate and create our variables
+    let queue = root ? [root] : [];
+    let result = [];
 
-    let result = []
-    let q = [root]
+    // standard while loop of queue
+    while (queue.length > 0) {
+        // create two arrays for current level and next level
+        let currLvl = [];
+        let nextLvl = [];
 
+        // This is the inner for loop we need to use to differentiate between current level and next level
+        queue.forEach((node) => {
+            currLvl.push(node.val);
+            // enque the children of current node value
+            if (node.left) nextLvl.push(node.left);
+            if (node.right) nextLvl.push(node.right);
+        });
 
-    while (q.length > 0) {
-        let currLvl = []
-        let nextLvl = []
+        // queue is the nextlvl and we push our current levels into result array.
+        queue = nextLvl;
+        result.push(currLvl);
+    };
 
-        q.forEach((node) => {
-            currLvl.push(node.val)
-            if (node.left) nextLvl.push(node.left)
-            if (node.right) nextLvl.push(node.right)
-        })
-
-        q = nextLvl
-        result.push(currLvl)
-    }
-
-    return result
+    return result;
 };
