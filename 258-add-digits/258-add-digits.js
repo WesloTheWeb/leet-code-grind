3291ccc47ee7422f3e7d145aee1dba324350d1e0
Wesley Webster <wesleyadwebster@gmail.com>
2022-09-09 00:42:19 -0500

    Time: 164 ms (5.50%), Space: 43.9 MB (59.83%) - LeetHub

--- 258-add-digits/258-add-digits.js
+++ 258-add-digits/258-add-digits.js
@@ -2,10 +2,27 @@
  * @param {number} num
  * @return {number}
  */
+/*
+1. create sum variable.
+2. while loop for sum is greater than or equal to 10
+    a. Add num % 10
+    b. Num divide by 10
+    c. If num is less than 10.
+        i. Add sum to num
+        ii. Set sum to 0.
+3. Return num.
+*/
 var addDigits = function(num) {
-    num = num.toString();
-    while (num.length !== 1) {
-        num = num.split('').reduce((a,i)=>+a + +i).toString();
-    }  
+    let sum = 0;
+    
+    while (num >= 10) {
+        sum += num % 10;
+        num = Math.floor(num / 10);
+        
+        if (num < 10) {
+            num += sum
+            sum = 0;
+        };
+    };
     return num;
 };
