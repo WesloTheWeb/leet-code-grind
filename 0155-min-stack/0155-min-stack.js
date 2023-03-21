25455304342eae65c6694380439c5f674c07b314
Wesley Webster <wesleyadwebster@gmail.com>
2023-03-21 00:21:50 -0500

    Time: 86 ms (99.68%), Space: 49.2 MB (91.84%) - LeetHub

---
+++ 0155-min-stack/0155-min-stack.js
@@ -0,0 +1,36 @@
+
+var MinStack = function() {
+    this.stack = [];
+    this.minStack = [];
+};
+
+MinStack.prototype.push = function(val) {
+    this.stack.push(val);
+    if (this.minStack.length === 0 || val <= this.minStack[this.minStack.length - 1]) {
+        this.minStack.push(val);
+    }
+};
+
+MinStack.prototype.pop = function() {
+    const val = this.stack.pop();
+    if (val === this.minStack[this.minStack.length - 1]) {
+        this.minStack.pop();
+    }
+};
+
+MinStack.prototype.top = function() {
+    return this.stack[this.stack.length - 1];
+};
+
+MinStack.prototype.getMin = function() {
+    return this.minStack[this.minStack.length - 1];
+};
+
+/** 
+ * Your MinStack object will be instantiated and called as such:
+ * var obj = new MinStack()
+ * obj.push(val)
+ * obj.pop()
+ * var param_3 = obj.top()
+ * var param_4 = obj.getMin()
+ */
