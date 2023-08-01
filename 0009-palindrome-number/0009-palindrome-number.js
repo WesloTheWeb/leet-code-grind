13f46316a5e0954a12b927fb3e65cebdbc220b11
Wesley Webster <wesleyadwebster@gmail.com>
2023-08-01 10:59:16 -0500

    Time: 138 ms (97.83%), Space: 51.2 MB (52.57%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.js
+++ 0009-palindrome-number/0009-palindrome-number.js
@@ -3,16 +3,16 @@
  * @return {boolean}
  */
 var isPalindrome = function(x) {
+    // String Approach
     let str = x.toString();
     
     let left = 0;
     let right = str.length - 1;
     
-    
-    for (let i = 0; i < str.length; i++) {
-      if (str[left] !== str[right]) {
-          return false;
-      };
+    while (left < right) {
+        if (str[left] !== str[right]) {
+            return false;      
+        };
         
         left++;
         right--;
