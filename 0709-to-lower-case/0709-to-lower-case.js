cd739c935a154a572ff9c97d1ffed0ad13b5f34a
Wesley Webster <wesleyadwebster@gmail.com>
2023-09-07 08:36:33 -0500

    Time: 48 ms (80.65%), Space: 41.6 MB (77.04%) - LeetHub

---
+++ 0709-to-lower-case/0709-to-lower-case.js
@@ -0,0 +1,10 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var toLowerCase = function(s) {
+    return s.toLowerCase();
+};
+
+// Time: O(n), iterate over the array once.
+// Space: O(n) where n is the length of the array.
