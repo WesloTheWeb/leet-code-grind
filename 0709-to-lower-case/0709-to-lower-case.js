c1a56e1fef2a49dc9f4872367119e24a3906b4f0
Wesley Webster <wesleyadwebster@gmail.com>
2023-09-07 08:47:57 -0500

    Time: 55 ms (49.52%), Space: 42.2 MB (24.76%) - LeetHub

--- 0709-to-lower-case/0709-to-lower-case.js
+++ 0709-to-lower-case/0709-to-lower-case.js
@@ -3,7 +3,21 @@
  * @return {string}
  */
 var toLowerCase = function(s) {
-    return s.toLowerCase();
+    let result = '';
+
+    for (let i = 0; i < s.length; i++) {
+        // moves across the string and get the character code of the letter
+        let charCode = s.charCodeAt(i);
+
+        if (charCode >= 65 && charCode <= 90) {
+            result += String.fromCharCode(charCode + 32); 
+            // 32 is difference between uppercase and lower caser
+        } else {
+            result += s.charAt(i);
+        };
+    };
+
+    return result;
 };
 
 // Time: O(n), iterate over the array once.
